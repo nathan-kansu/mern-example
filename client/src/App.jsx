ec8af518b75efc100bfdb608b3578b0d2a452808
Nathan Tredgett <nathantredgett@gmail.com>
2020-05-18 19:40:28 +0100

    added Home page

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useReducer, useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Main from "./components/Main";
+import PrivateRoute from "./components/PrivateRoute";
+import Categories from "./contexts/Categories";
+import LoggedIn from "./contexts/LoggedIn";
+import categoriesReducer from "./reducers/categories";
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [state, dispatch] = useReducer(categoriesReducer, { categories: [] });
+  const loggedInState = { isLoggedIn, setIsLoggedIn };
+  const categoriesState = { state, dispatch };
+
+  return (
+    <LoggedIn.Provider value={loggedInState}>
+      <Categories.Provider value={categoriesState}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <Container>
+              <Header />
+              <Main>
+                <Switch>
+                  <PrivateRoute component={Home} exact path="/" />
+                  <Route component={Login} path="/login" />
+                </Switch>
+              </Main>
+            </Container>
+          </BrowserRouter>
+        </ThemeProvider>
+      </Categories.Provider>
+    </LoggedIn.Provider>
+  );
+};
+
+export default App;
---
+++ client/src/components/Home/index.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useContext } from "react";
+import io from "socket.io-client";
+import Heading from "../Heading";
+import Loading from "../Loading";
+import Categories from "../Categories";
+import { getCategories } from "../../api/categories";
+import {
+  CATEGORIES_CREATE,
+  CATEGORIES_GET,
+  CATEGORIES_DELETE,
+  CATEGORIES_UPDATE,
+} from "../../reducers/categories";
+import CategoriesContext from "../../contexts/Categories";
+
+const Home = () => {
+  const { state, dispatch } = useContext(CategoriesContext);
+
+  useEffect(() => {
+    const socket = io.connect("http://localhost:9000");
+
+    socket.on("category:delete", (data) => {
+      const { id } = data;
+      dispatch({ type: CATEGORIES_DELETE, payload: id });
+    });
+
+    socket.on("category:update", ({ _id, label }) =>
+      dispatch({ type: CATEGORIES_UPDATE, payload: { _id, label } })
+    );
+
+    socket.on("category:created", ({ _id, label, parent }) => {
+      dispatch({ type: CATEGORIES_CREATE, payload: { _id, label, parent } });
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const categories = await getCategories();
+      dispatch({ type: CATEGORIES_GET, payload: categories });
+    };
+    fetchCategories();
+  }, [dispatch]);
+
+  return (
+    <>
+      <Heading>Categories</Heading>
+      {!state.categories.length ? (
+        <Loading />
+      ) : (
+        <Categories parentCategory={null} />
+      )}
+    </>
+  );
+};
+
+export default Home;
