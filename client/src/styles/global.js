d345a1ee7fbe1429d880ce7528b7b9fde451801d
Nathan Tredgett <nathantredgett@gmail.com>
2020-05-18 15:19:21 +0100

    added styled-components global styling and themes

---
+++ client/src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  body {
+    color: ${({ theme }) => theme.color.white};
+    font-family: Roboto;
+    background: linear-gradient(20deg, ${({ theme }) => theme.color.pink}, ${({
+  theme,
+}) => theme.color.orange});
+  }
+`;
+export default GlobalStyle;
---
+++ client/src/styles/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  color: {
+    orange: "#daa357",
+    pink: "#db7093",
+    white: "#fff",
+  },
+};
+
+export default theme;
